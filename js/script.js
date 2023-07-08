10d0daf24081b4e82a2b12f3716f6b584318b9c1
smailikin70 <107893164+smailikin70@users.noreply.github.com>
2023-07-08 17:17:03 +0300

    GRID CARD TEACHER TRUBBLES

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ tabsBtn.forEach(function (element) {
         document.querySelector(`[data-target="${path}"]`).classList.add('pages_active');
     })
 })
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+    // options
+});
