939073cc1763d6f814889c1b6dffa47a88e4667e
smailikin70 <107893164+smailikin70@users.noreply.github.com>
2023-07-16 15:02:00 +0300

    swiper

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
-const tabsBtn = document.querySelectorAll('.nav_link');
-const tabsItem = document.querySelectorAll('.pages');
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
 
-tabsBtn.forEach(function (element) {
-    element.addEventListener('click', function (e) {
-        const path = e.currentTarget.dataset.path;
+    loop: true,
 
-        tabsBtn.forEach(function (btn) {
-            btn.classList.remove('nav_link_active')
-        });
-        e.currentTarget.classList.add('nav_link_active');
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-        tabsItem.forEach(function (element) {
-            element.classList.remove('pages_active')
-        });
-        document.querySelector(`[data-target="${path}"]`).classList.add('pages_active');
-    })
-})
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-    // options
-    itemSelector: '.grid-item',
-    columnWidth: 200
-});
-
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-    // options
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
