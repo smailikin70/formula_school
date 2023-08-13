7e64ca26dd9d62ba753863449d1d31285b40af9f
smailikin70 <107893164+smailikin70@users.noreply.github.com>
2023-08-13 20:45:59 +0300

    hover + focus
    question - SWIPER JS adaptive?!

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,15 +7,50 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     slidesPerView: 2,
     autoplay: 200,
-    // centeredSlides: true,
-    // freeMode: true,
-    autoHeight: true,
+    autoHeight: false,
+
 });
+
+if (document.documentElement.clientWidht < 1150){
+    const swiper, {
+        slidesPerView: 1,
+    }
+},
 
 new Accordion('.accordion-container');
 
-function ChangeProg() {
-    const selectElement = document.getElementById('Prog');
+function ChangeProgram() {
+    const selectElement = document.getElementById('Program');
     const selectedProg = selectElement.value;
     alert('Вы выбрали: ' + selectedProg);
+};
+
+function ChangeLesson() {
+    const selectElement = document.getElementById('Lesson');
+    const selectedProg = selectElement.value;
+    alert('Вы выбрали: ' + selectedProg);
+};
+
+function ChangeClass() {
+    const selectElement = document.getElementById('class-age');
+    const selectedProg = selectElement.value;
+    alert('Вы выбрали: ' + selectedProg);
+};
+
+function ChangeAddress() {
+    const selectElement = document.getElementById('address');
+    const selectedProg = selectElement.value;
+    alert('Вы выбрали: ' + selectedProg);
+};
+
+$(document).ready(function () {
+    PopUpHide();
+});
+
+function PopUpShow() {
+    $("#popup1").show();
+}
+
+function PopUpHide() {
+    $("#popup1").hide();
 }
